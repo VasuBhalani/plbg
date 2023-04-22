301c331928ff42d32aca3b7d20edb858951c2022
Vasu <vasubhalani2004@gmail.com>
2023-04-22 20:08:46 +0530

    all are commited

---
+++ diff2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+    int a=9,b=7;
+    cout<<a-b;
+return 0;
+}
---
+++ mul3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std ;
+// class
+// {
+// };
+int main()
+{ int a=5,b=7;
+cout<<a*b;
+return 0;
+}
